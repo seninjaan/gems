62db0217b76e0b9548e7905dfbd2ec14ae29e3ff
unknown <seanegay@gmail.com>
2020-04-12 13:29:01 -0500

    init commit using pygame template and gems sprite sheet

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # gems
 A basic gem game. Maybe.
+
+
+
+### Credit
+[NES Gems by R3tr0BoiDX](https://opengameart.org/content/nes-gems)
---
+++ unit_tests/test_game.py
@@ -0,0 +1,11 @@
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    # Arrange
+
+    # Act
+
+    # Assert
+    assert func(3) == 4
